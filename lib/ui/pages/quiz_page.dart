7e0de2ea26f4a86d646c54c16e9ef5c57d1de5ec
Damodar Lohani <dlohani48@gmail.com>
2020-07-09 17:40:05 +0545

    Increase fontsize in quiz page based on screen width

--- lib/ui/pages/quiz_page.dart
+++ lib/ui/pages/quiz_page.dart
@@ -9,7 +9,8 @@ class QuizPage extends StatefulWidget {
   final List<Question> questions;
   final Category category;
 
-  const QuizPage({Key key, @required this.questions, this.category}) : super(key: key);
+  const QuizPage({Key key, @required this.questions, this.category})
+      : super(key: key);
 
   @override
   _QuizPageState createState() => _QuizPageState();
@@ -17,25 +18,21 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   final TextStyle _questionStyle = TextStyle(
-    fontSize: 18.0,
-    fontWeight: FontWeight.w500,
-    color: Colors.white
-  );
+      fontSize: 18.0, fontWeight: FontWeight.w500, color: Colors.white);
 
   int _currentIndex = 0;
-  final Map<int,dynamic> _answers = {};
+  final Map<int, dynamic> _answers = {};
   final GlobalKey<ScaffoldState> _key = GlobalKey<ScaffoldState>();
 
-
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     Question question = widget.questions[_currentIndex];
     final List<dynamic> options = question.incorrectAnswers;
-    if(!options.contains(question.correctAnswer)) {
+    if (!options.contains(question.correctAnswer)) {
       options.add(question.correctAnswer);
       options.shuffle();
     }
-    
+
     return WillPopScope(
       onWillPop: _onWillPop,
       child: Scaffold(
@@ -49,9 +46,8 @@ class _QuizPageState extends State<QuizPage> {
             ClipPath(
               clipper: WaveClipperTwo(),
               child: Container(
-                decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor
-                ),
+                decoration:
+                    BoxDecoration(color: Theme.of(context).primaryColor),
                 height: 200,
               ),
             ),
@@ -63,32 +59,40 @@ class _QuizPageState extends State<QuizPage> {
                     children: <Widget>[
                       CircleAvatar(
                         backgroundColor: Colors.white70,
-                        child: Text("${_currentIndex+1}"),
+                        child: Text("${_currentIndex + 1}"),
                       ),
                       SizedBox(width: 16.0),
                       Expanded(
-                        child: Text(HtmlUnescape().convert(widget.questions[_currentIndex].question),
+                        child: Text(
+                          HtmlUnescape().convert(
+                              widget.questions[_currentIndex].question),
                           softWrap: true,
-                          style: _questionStyle,),
+                          style: MediaQuery.of(context).size.width > 800
+                              ? _questionStyle.copyWith(fontSize: 30.0)
+                              : _questionStyle,
+                        ),
                       ),
                     ],
                   ),
-
                   SizedBox(height: 20.0),
                   Card(
                     child: Column(
                       mainAxisSize: MainAxisSize.min,
                       children: <Widget>[
-                        ...options.map((option)=>RadioListTile(
-                          title: Text(HtmlUnescape().convert("$option")),
-                          groupValue: _answers[_currentIndex],
-                          value: option,
-                          onChanged: (value){
-                            setState(() {
-                              _answers[_currentIndex] = option;
-                            });
-                          },
-                        )),
+                        ...options.map((option) => RadioListTile(
+
+                              title: Text(HtmlUnescape().convert("$option"),style: MediaQuery.of(context).size.width > 800
+                              ? TextStyle(
+                                fontSize: 30.0
+                              ) : null,),
+                              groupValue: _answers[_currentIndex],
+                              value: option,
+                              onChanged: (value) {
+                                setState(() {
+                                  _answers[_currentIndex] = option;
+                                });
+                              },
+                            )),
                       ],
                     ),
                   ),
@@ -96,7 +100,13 @@ class _QuizPageState extends State<QuizPage> {
                     child: Container(
                       alignment: Alignment.bottomCenter,
                       child: RaisedButton(
-                        child: Text( _currentIndex == (widget.questions.length - 1) ? "Submit" : "Next"),
+                        padding: MediaQuery.of(context).size.width > 800
+                              ? const EdgeInsets.symmetric(vertical: 20.0,horizontal: 64.0) : null,
+                        child: Text(
+                            _currentIndex == (widget.questions.length - 1)
+                                ? "Submit"
+                                : "Next", style: MediaQuery.of(context).size.width > 800
+                              ? TextStyle(fontSize: 30.0) : null,),
                         onPressed: _nextSubmit,
                       ),
                     ),
@@ -111,46 +121,46 @@ class _QuizPageState extends State<QuizPage> {
   }
 
   void _nextSubmit() {
-    if(_answers[_currentIndex] == null) {
+    if (_answers[_currentIndex] == null) {
       _key.currentState.showSnackBar(SnackBar(
         content: Text("You must select an answer to continue."),
       ));
       return;
     }
-    if(_currentIndex < (widget.questions.length - 1)){
+    if (_currentIndex < (widget.questions.length - 1)) {
       setState(() {
-          _currentIndex++;
+        _currentIndex++;
       });
     } else {
       Navigator.of(context).pushReplacement(MaterialPageRoute(
-        builder: (_) => QuizFinishedPage(questions: widget.questions, answers: _answers)
-      ));
+          builder: (_) => QuizFinishedPage(
+              questions: widget.questions, answers: _answers)));
     }
   }
 
   Future<bool> _onWillPop() async {
     return showDialog<bool>(
-      context: context,
-      builder: (_) {
-        return AlertDialog(
-          content: Text("Are you sure you want to quit the quiz? All your progress will be lost."),
-          title: Text("Warning!"),
-          actions: <Widget>[
-            FlatButton(
-              child: Text("Yes"),
-              onPressed: (){
-                Navigator.pop(context,true);
-              },
-            ),
-            FlatButton(
-              child: Text("No"),
-              onPressed: (){
-                Navigator.pop(context,false);
-              },
-            ),
-          ],
-        );
-      }
-    );
+        context: context,
+        builder: (_) {
+          return AlertDialog(
+            content: Text(
+                "Are you sure you want to quit the quiz? All your progress will be lost."),
+            title: Text("Warning!"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("Yes"),
+                onPressed: () {
+                  Navigator.pop(context, true);
+                },
+              ),
+              FlatButton(
+                child: Text("No"),
+                onPressed: () {
+                  Navigator.pop(context, false);
+                },
+              ),
+            ],
+          );
+        });
   }
 }
